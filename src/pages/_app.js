5847a2bcb91782b2b11314f9dc32afd320a928c4
Karan_Paul <karan.paul@fnp.com>
2023-10-05 11:58:44 +0530

    First commit for news app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+const poppins = Poppins({
+  weight: ["400", "500", "600"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
