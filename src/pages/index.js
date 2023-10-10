446dac21b8ce5091ec52a37aceecd2b9c58dd16e
Karan_Paul <karan.paul@fnp.com>
2023-10-10 14:26:29 +0530

    testing agian

--- src/pages/index.js
+++ src/pages/index.js
@@ -95,6 +95,8 @@ const Home = () => {
 //     await store.dispatch(homeData());
 //   }
 // });
+
+
 Home.getInitialProps = wrapper.getInitialPageProps((store) => async (ctx) => {
   if (ctx.req) {
     await store.dispatch(homeData());
