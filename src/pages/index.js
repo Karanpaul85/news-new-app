1396c884a79c675850c642118bfba9e60f7e3895
Karan_Paul <karan.paul@fnp.com>
2023-10-10 20:42:55 +0530

    index.js indenting

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,9 +54,7 @@ const Home = () => {
   };
   return (
     <Layout>
-      <Head>
-        {ogMetaTags( newsData[0])}
-      </Head>
+      <Head>{ogMetaTags(newsData[0])}</Head>
       <div style={{ height: 200 }}>Slider</div>
       <div className={styles.mainHeading}>
         <h1>{textConst.LATEST_NEWS}</h1>
