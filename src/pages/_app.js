a88f3e8c996220641997f06fe0ae24fe8013a60c
Karan_Paul <karan.paul@fnp.com>
2023-10-14 10:24:35 +0530

    downgrad the nextjs version 13.1.0

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,23 @@
 import { wrapper } from "../redux/store";
 import "@/styles/globals.css";
-import { Poppins } from "next/font/google";
+//import { Poppins } from "next/font/google";
 import { Provider } from "react-redux";
-const poppins = Poppins({
-  weight: ["400", "500", "600"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "swap",
-});
+// const poppins = Poppins({
+//   weight: ["400", "500", "600"],
+//   style: ["normal", "italic"],
+//   subsets: ["latin"],
+//   display: "swap",
+// });
+
 const App = ({ Component, ...rest }) => {
   const { store, props } = wrapper.useWrappedStore(rest);
   return (
     <>
-      <style jsx global>{`
+      {/* <style jsx global>{`
         html {
           font-family: ${poppins.style.fontFamily};
         }
-      `}</style>
+      `}</style> */}
       <Provider store={store}>
         <Component {...props.pageProps} />
       </Provider>
